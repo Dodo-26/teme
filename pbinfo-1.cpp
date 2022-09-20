e4c493ef6ea3d0bb87118283358dd23b71b2c870
Dodo-26 <dumitru.aian@gmail.com>
2022-09-20 15:37:02 +0300

    homework

--- pbinfo-1.cpp
+++ pbinfo-1.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 int main(){
-
+    unsigned num1,num2;
+    cin>> num1 >> num2;
+    if (num1 > 1000000000 || num2 > 1000000000){
+        cout<< "The numbers are too big"<< endl;
+    }
+    cout<< num1+num2;
     return 0;
 }
---
+++ pbinfo-2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string urare = "Sarbatori fericite tuturor celor dragi";
+    cout<< urare<< endl;
+    return 0;
+}
